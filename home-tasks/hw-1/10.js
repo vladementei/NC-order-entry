1c78cad6e59d6fa5515b566032918aa4c034ac68
Vlad Dementei <vladementei@gmail.com>
2019-10-17 06:45:34 +0200

    feat: upgrade 10th script

--- home-tasks/hw-1/10.js
+++ home-tasks/hw-1/10.js
@@ -1,17 +1,14 @@
 function triangle(h) {
-  console.log('^');
-  let str = '';
-  for (let i = 0; i < h - 2; i++){
-    let str = '';
-    for(let j = 0; j < i + 1; j++){
-      str += ' ';
+  for(let i = 0; i < h; i++){
+    let empty = '';
+    let triangles = '^';
+    for(let j = 0; j < h - i - 1; j++){
+      empty += ' ';
     }
-    console.log('^' + str + '^');
+    for(let j = 0; j < i; j++){
+      triangles += '^^';
+    }
+    console.log(empty + triangles + empty);
   }
-  for(let j = 0; j < h + 1; j++){
-    str += '^';
-  }
-  console.log(str);
 }
-
-triangle(10);
+triangle(7);
