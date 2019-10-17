54f572b14ccae1fdf46330d9a031193d37ba3e98
Vlad Dementei <vladementei@gmail.com>
2019-10-17 07:54:08 +0200

    feat: stream 14th script

--- home-tasks/hw-1/14.js
+++ home-tasks/hw-1/14.js
@@ -1,17 +1,7 @@
 let students = [{ id: 2, name: 'John', age: 28}, { id: 3, name: 'William', age: 34}, { id: 4, name: 'Graham', age: 27},
   { id: 5, name: 'Oliver', age: 28}];
-let first = [];
-for (let i = 0; i < students.length; i++){
-  if((students[i].name.toLowerCase().indexOf("am") >= 0) && (students[i].age <= 30)){
-    first.push(students[i]);
-  }
-}
-let second = [];
-for (let i = 0; i < students.length; i++){
-  if((students[i].name.toLowerCase().indexOf("am") >= 0) && (students[i].age <= 30)){
-    second.push(students[i]);
-    break;
-  }
-}
+let first = students.filter(elem => (elem.name.toLowerCase().indexOf('am') >= 0) && (elem.age <= 30));
+let second = students.find(elem => (elem.name.toLowerCase().indexOf('am') >= 0) && (elem.age <= 30));
+
 console.log(first);
 console.log(second);
