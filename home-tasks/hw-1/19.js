aa20868f7edda70cb9491676d71a41d8ce417103
Vlad Dementei <vladementei@gmail.com>
2019-10-17 17:19:37 +0200

    perf: 19th first and last elem of sorted array is answer

--- home-tasks/hw-1/19.js
+++ home-tasks/hw-1/19.js
@@ -1,5 +1,5 @@
 //19) Условие: дан целочисленный массив. Задание: Отсортировать массив в обратном порядке. Найти min и max.
 let arr = [3, -2, 6, 1, 3, 30, -8, 1];
 console.log(arr.sort((o1,o2) => o2 - o1));
-console.log(Math.min(...arr));
-console.log(Math.max(...arr));
+console.log(arr[arr.length-1]);
+console.log(arr[0]);
