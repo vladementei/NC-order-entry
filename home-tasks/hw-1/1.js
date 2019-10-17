a3d36fc8fab2a92be1d97583569ae35fc5196590
Vlad Dementei <vladementei@gmail.com>
2019-10-17 17:10:03 +0200

    fix: 1st return null if no numbers

--- home-tasks/hw-1/1.js
+++ home-tasks/hw-1/1.js
@@ -1,14 +1,16 @@
 //1) Написать функцию принимающую параметром массив, и возвращающую объект вида { maxValue: ${maxValue}, minValue: ${minValue} }
 //    Под минимимальным и максимальным значением подрузамевается число. Учесть то что массив может состоять не только из чисел.
 function minMax (arr) {
-  let min = arr[0];
-  let max = arr[0];
+  let min = null;
+  let max = null;
   for (let elem of arr) {
     if (typeof elem == "number") {
-      if (elem < min) {
+      if(min === null || max === null){
+        max = elem;
         min = elem;
-      }
-      if (elem > max) {
+      }else if (elem < min) {
+        min = elem;
+      }else if (elem > max) {
         max = elem;
       }
     }
@@ -16,4 +18,6 @@ function minMax (arr) {
   return {maxValue: max, minValue: min};
 }
 let arr = [1,5.6, 'w', true, null, undefined, 5, 9.7, -4, 3];
+console.log(minMax(arr));
+arr = ['u', 'w', true, null, undefined];
 console.log(minMax(arr));
