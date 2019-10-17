f25281646e25c289451ddadb06a52a5294688978
Vlad Dementei <vladementei@gmail.com>
2019-10-17 08:43:48 +0200

    feat: 1st script

---
+++ home-tasks/hw-1/1.js
@@ -0,0 +1,17 @@
+function minMax (arr) {
+  let min = arr[0];
+  let max = arr[0];
+  for (let elem of arr) {
+    if (typeof elem == "number") {
+      if (elem < min) {
+        min = elem;
+      }
+      if (elem > max) {
+        max = elem;
+      }
+    }
+  }
+  return {maxValue: max, minValue: min};
+}
+let arr = [1,5.6, 'w', true, null, undefined, 5, 9.7, -4, 3];
+console.log(minMax(arr));
