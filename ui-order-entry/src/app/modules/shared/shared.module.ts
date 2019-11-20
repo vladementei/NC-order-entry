1551edf4b22523a6ff0014d4687e4ded9c048df1
Vlad Dementei <vladementei@gmail.com>
2019-11-20 18:55:21 +0200

    feat: filter design

--- ui-order-entry/src/app/modules/shared/shared.module.ts
+++ ui-order-entry/src/app/modules/shared/shared.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 import {MatButtonModule, MatCardModule, MatDialogModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import {DialogComponent} from '../../components/dialog/dialog.component';
 import {RoleGuard} from '../../guards/role.guard';
 
@@ -17,7 +18,8 @@ const materialModules = [
   MatInputModule,
   MatCardModule,
   MatButtonModule,
-  MatDialogModule
+  MatDialogModule,
+  MatCheckboxModule
 ];
 
 @NgModule({
