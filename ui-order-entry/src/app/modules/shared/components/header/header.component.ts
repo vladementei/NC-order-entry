b66b751055c432db90113ae5635f1b2deffd44ec
Vlad Dementei <vladementei@gmail.com>
2019-12-11 08:20:29 +0200

    feat: log out

--- ui-order-entry/src/app/modules/shared/components/header/header.component.ts
+++ ui-order-entry/src/app/modules/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UserModel} from '../../../../models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
+  user: UserModel;
   constructor() {
+    this.user = JSON.parse(localStorage.getItem('user_info'));
+    console.log(this.user);
   }
 
   ngOnInit(): void {
+  }
+
+  logOut() {
+    localStorage.removeItem('user_info');
+    window.location.reload();
   }
 }
